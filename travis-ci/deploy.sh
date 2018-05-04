b50fce459a4606c371b23a4554180b59871d18c1
Daniel De Luca <daniel.deluca@ibanity.com>
2018-05-04 15:41:07 +0200

    Travic CI Integration and release to maven central

--- travis-ci/deploy.sh
+++ travis-ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ]; then
+if [ "$TRAVIS_BRANCH" = 'master' ] || [ -n "$TRAVIS_TAG" ]; then
     if [ -z "$TRAVIS_TAG" ]; then
         mvn deploy --settings travis-ci/maven_settings.xml -Dchangelist='-SNAPSHOT'
     else
