9ebdb90cbbc0231f6838b7291f8759d634b9f944
Damien Bravin <damien.bravin@inadevs.be>
2019-05-08 15:43:42 +0200

    Add Synchronization resource.

---
+++ src/test/java/com/ibanity/samples/customer/SynchronizationSample.java
@@ -0,0 +1,34 @@
+package com.ibanity.samples.customer;
+
+import com.ibanity.apis.client.models.CustomerAccessToken;
+import com.ibanity.apis.client.models.Synchronization;
+import com.ibanity.apis.client.models.factory.read.SynchronizationReadQuery;
+import com.ibanity.apis.client.services.SynchronizationService;
+import com.ibanity.apis.client.services.impl.SynchronizationServiceImpl;
+
+import java.util.UUID;
+
+public class SynchronizationSample {
+
+    private SynchronizationService synchronizationService = new SynchronizationServiceImpl();
+
+    public Synchronization create(CustomerAccessToken customerAccessToken, UUID accountId) {
+        SynchronizationReadQuery synchronizationReadQuery =
+                SynchronizationReadQuery.builder()
+                        .resourceId(accountId.toString())
+                        .subtype("accountDetails")
+                        .resourceType("account")
+                        .customerAccessToken(customerAccessToken.getToken())
+                        .build();
+        return synchronizationService.create(synchronizationReadQuery);
+    }
+
+    public Synchronization find(CustomerAccessToken customerAccessToken, UUID synchronizationId) {
+        SynchronizationReadQuery synchronizationReadQuery =
+                SynchronizationReadQuery.builder()
+                        .synchronizationId(synchronizationId)
+                        .customerAccessToken(customerAccessToken.getToken())
+                        .build();
+        return synchronizationService.find(synchronizationReadQuery);
+    }
+}
