91c99a7805c112e80cf03fac9d6658b0afd71f2f
Damien Bravin <damien.bravin@inadevs.be>
2019-05-08 15:54:30 +0200

    Allow to fetch account list for Account information access request.

--- src/main/java/com/ibanity/apis/client/services/impl/AccountsServiceImpl.java
+++ src/main/java/com/ibanity/apis/client/services/impl/AccountsServiceImpl.java
@@ -2,6 +2,7 @@ package com.ibanity.apis.client.services.impl;
 
 import com.ibanity.apis.client.configuration.IbanityConfiguration;
 import com.ibanity.apis.client.models.Account;
+import com.ibanity.apis.client.models.AccountInformationAccessRequest;
 import com.ibanity.apis.client.models.FinancialInstitution;
 import com.ibanity.apis.client.models.factory.read.AccountReadQuery;
 import com.ibanity.apis.client.models.factory.read.AccountsReadQuery;
@@ -21,7 +22,7 @@ public class AccountsServiceImpl extends AbstractServiceImpl implements Accounts
 
     @Override
     public Account find(final AccountReadQuery accountReadQuery) {
-        return getRepository(accountReadQuery.getCustomerAccessToken(), accountReadQuery.getFinancialInstitutionId())
+        return getRepository(accountReadQuery.getCustomerAccessToken(), accountReadQuery.getFinancialInstitutionId(), null)
                 .findOne(accountReadQuery.getAccountId(), new QuerySpec(Account.class));
     }
 
@@ -37,14 +38,19 @@ public class AccountsServiceImpl extends AbstractServiceImpl implements Accounts
 
         return findAll(querySpec,
                 getRepository(accountsReadQuery.getCustomerAccessToken(),
-                        accountsReadQuery.getFinancialInstitutionId()));
+                        accountsReadQuery.getFinancialInstitutionId(),
+                        accountsReadQuery.getAccountInformationAccessRequestId()));
     }
 
     private ResourceRepositoryV2<Account, UUID> getRepository(
-            final String customerAccessToken, final UUID financialInstitutionId) {
+            final String customerAccessToken, final UUID financialInstitutionId, final UUID accountInformationAccessRequestId) {
 
         String finalPath;
-        if (financialInstitutionId != null) {
+        if (accountInformationAccessRequestId != null) {
+            finalPath = IbanityConfiguration.getApiUrls().getCustomer().getFinancialInstitution().getAccountInformationAccessRequest().getAccounts()
+                    .replace(FinancialInstitution.API_URL_TAG_ID, financialInstitutionId.toString())
+                    .replace(AccountInformationAccessRequest.API_URL_TAG_ID, financialInstitutionId.toString());
+        } else if (financialInstitutionId != null) {
             finalPath = IbanityConfiguration.getApiUrls().getCustomer().getFinancialInstitution().getAccounts()
                     .replace(FinancialInstitution.API_URL_TAG_ID, financialInstitutionId.toString());
         } else {
