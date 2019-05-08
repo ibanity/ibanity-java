f851c95a263c7ac8b5a4ea5be30631664b2941bd
Damien Bravin <damien.bravin@inadevs.be>
2019-05-08 15:50:49 +0200

    Add find method for Account information access request.

--- src/main/java/com/ibanity/apis/client/models/factory/create/AccountInformationAccessRequestCreationQuery.java
+++ src/main/java/com/ibanity/apis/client/models/factory/create/AccountInformationAccessRequestCreationQuery.java
@@ -1,5 +1,6 @@
 package com.ibanity.apis.client.models.factory.create;
 
+import com.ibanity.apis.client.paging.IbanityPagingSpec;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,10 +10,12 @@ import java.util.UUID;
 @Getter
 @Builder
 public final class AccountInformationAccessRequestCreationQuery {
+    private UUID accountInformationAccessRequestId;
     private String customerAccessToken;
     private UUID financialInstitutionId;
     private String redirectURI;
     private String consentReference;
     private UUID idempotencyKey;
     private List<String> requestedAccountReference;
+    private IbanityPagingSpec pagingSpec;
 }
