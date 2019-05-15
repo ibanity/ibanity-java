8afe288cb0552bfef12bd2fb44311c83adcff918
Damien Bravin <damien.bravin@inadevs.be>
2019-05-15 14:22:16 +0200

    Implement ApiUrlProvider.

---
+++ src/main/java/com/ibanity/apis/client/services/impl/ApiUrlProviderImpl.java
@@ -0,0 +1,55 @@
+package com.ibanity.apis.client.services.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.ibanity.apis.client.configuration.IbanityConfiguration;
+import com.ibanity.apis.client.network.http.client.IbanityHttpClient;
+import com.ibanity.apis.client.network.http.client.IbanityHttpUtils;
+import com.ibanity.apis.client.services.ApiUrlProvider;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.stream.Stream;
+
+import static java.lang.String.format;
+
+public class ApiUrlProviderImpl implements ApiUrlProvider {
+
+    private JsonNode apiUrls;
+    private final IbanityHttpClient ibanityHttpClient;
+
+    public ApiUrlProviderImpl(IbanityHttpClient ibanityHttpClient) {
+        this.ibanityHttpClient = ibanityHttpClient;
+    }
+
+    @Override
+    public String find(String... paths) {
+        if (apiUrls == null) {
+            throw new IllegalStateException("Api schema hasn't been loaded");
+        }
+        try {
+            return Stream.of(paths)
+                    .reduce(apiUrls, JsonNode::get, (jsonNode1, jsonNode2) -> jsonNode2)
+                    .textValue();
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Url cannot be found");
+        }
+    }
+
+    @Override
+    public void loadApiSchema() {
+        String ibanityApiUrl = IbanityConfiguration.getConfiguration(IbanityConfiguration.IBANITY_API_ENDPOINT_PROPERTY_KEY);
+        try {
+            String schema = ibanityHttpClient.get(new URI(ibanityApiUrl + "/xs2a"), null);
+            apiUrls = mapJsonToMap(schema);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(format("Cannot create api schema URI for string %s", ibanityApiUrl), e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Cannot parse api schema", e);
+        }
+    }
+
+    private JsonNode mapJsonToMap(String schema) throws IOException {
+        return IbanityHttpUtils.objectMapper().readTree(schema).get("links");
+    }
+}
---
+++ src/test/java/com/ibanity/apis/client/services/impl/ApiUrlProviderImplTest.java
@@ -0,0 +1,86 @@
+package com.ibanity.apis.client.services.impl;
+
+import com.ibanity.apis.client.network.http.client.IbanityHttpClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ApiUrlProviderImplTest {
+
+    @Mock
+    private IbanityHttpClient ibanityHttpClient;
+
+    @InjectMocks
+    private ApiUrlProviderImpl apiUrlProvider;
+
+    @Test
+    void find() {
+        loadSchema();
+        String actual = apiUrlProvider.find("customer", "financialInstitution", "accountInformationAccessRequest", "accounts");
+        assertThat(actual).isEqualTo("https://api.ibanity.localhost/xs2a/customer/financial-institutions/{financialInstitutionId}/account-information-access-requests/{accountInformationAccessRequestId}/accounts");
+    }
+
+    @Test
+    void find_whenPathNotFound_throwIllegalArgumentException() {
+        loadSchema();
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            apiUrlProvider.find("customer", "financiulInstitution", "accountInformationAccessRequest", "accounts");
+        });
+    }
+
+    @Test
+    void find_whenSchemaNotLoaded_throwAnError() {
+        assertThatExceptionOfType(IllegalStateException.class).isThrownBy(() -> {
+            apiUrlProvider.find("customer", "financialInstitution", "accountInformationAccessRequest", "accounts");
+        });
+    }
+
+    private void loadSchema() {
+        when(ibanityHttpClient.get(any(), eq(null))).thenReturn(getSchema());
+        apiUrlProvider.loadApiSchema();
+    }
+
+    private String getSchema() {
+        //language=JSON
+        return "{\n" +
+                "  \"links\": {\n" +
+                "    \"customer\": {\n" +
+                "      \"accounts\": \"https://api.ibanity.localhost/xs2a/customer/accounts\",\n" +
+                "      \"financialInstitution\": {\n" +
+                "        \"accountInformationAccessRequest\": {\n" +
+                "          \"accounts\": \"https://api.ibanity.localhost/xs2a/customer/financial-institutions/{financialInstitutionId}/account-information-access-requests/{accountInformationAccessRequestId}/accounts\"\n" +
+                "        },\n" +
+                "        \"accountInformationAccessRequests\": \"https://api.ibanity.localhost/xs2a/customer/financial-institutions/{financialInstitutionId}/account-information-access-requests/{accountInformationAccessRequestId}\",\n" +
+                "        \"accounts\": \"https://api.ibanity.localhost/xs2a/customer/financial-institutions/{financialInstitutionId}/accounts/{accountId}\",\n" +
+                "        \"paymentInitiationRequests\": \"https://api.ibanity.localhost/xs2a/customer/financial-institutions/{financialInstitutionId}/payment-initiation-requests/{paymentInitiationRequestId}\",\n" +
+                "        \"transactions\": \"https://api.ibanity.localhost/xs2a/customer/financial-institutions/{financialInstitutionId}/accounts/{accountId}/transactions/{transactionId}\"\n" +
+                "      },\n" +
+                "      \"financialInstitutions\": \"https://api.ibanity.localhost/xs2a/customer/financial-institutions\",\n" +
+                "      \"self\": \"https://api.ibanity.localhost/xs2a/customer\",\n" +
+                "      \"synchronizations\": \"https://api.ibanity.localhost/xs2a/customer/synchronizations/{synchronizationId}\"\n" +
+                "    },\n" +
+                "    \"customerAccessTokens\": \"https://api.ibanity.localhost/xs2a/customer-access-tokens\",\n" +
+                "    \"financialInstitutions\": \"https://api.ibanity.localhost/xs2a/financial-institutions/{financialInstitutionId}\",\n" +
+                "    \"sandbox\": {\n" +
+                "      \"financialInstitution\": {\n" +
+                "        \"financialInstitutionAccount\": {\n" +
+                "          \"financialInstitutionTransactions\": \"https://api.ibanity.localhost/sandbox/financial-institutions/{financialInstitutionId}/financial-institution-users/{financialInstitutionUserId}/financial-institution-accounts/{financialInstitutionAccountId}/financial-institution-transactions/{financialInstitutionTransactionId}\"\n" +
+                "        },\n" +
+                "        \"financialInstitutionAccounts\": \"https://api.ibanity.localhost/sandbox/financial-institutions/{financialInstitutionId}/financial-institution-users/{financialInstitutionUserId}/financial-institution-accounts/{financialInstitutionAccountId}\"\n" +
+                "      },\n" +
+                "      \"financialInstitutionUsers\": \"https://api.ibanity.localhost/sandbox/financial-institution-users/{financialInstitutionUserId}\",\n" +
+                "      \"financialInstitutions\": \"https://api.ibanity.localhost/sandbox/financial-institutions/{financialInstitutionId}\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+    }
+}
