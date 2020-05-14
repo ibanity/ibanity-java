f7de78ebda01392d39f8d6ab93546c531b025611
Damien Bravin <damien.bravin@inadevs.be>
2020-05-14 16:26:41 +0200

    Add AllowMultycurrencyAccounts property in order to support multicurrency accounts.

--- src/main/java/com/ibanity/apis/client/products/xs2a/models/create/AccountInformationAccessRequestCreationQuery.java
+++ src/main/java/com/ibanity/apis/client/products/xs2a/models/create/AccountInformationAccessRequestCreationQuery.java
@@ -28,6 +28,7 @@ public final class AccountInformationAccessRequestCreationQuery {
     private String customerIpAddress;
     private boolean allowFinancialInstitutionRedirectUri;
     private boolean skipIbanityCompletionCallback;
+    private boolean allowMulticurrencyAccounts;
     private String state;
 
     private IbanityPagingSpec pagingSpec;
