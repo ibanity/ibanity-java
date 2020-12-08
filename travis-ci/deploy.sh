c15ab2b9382eaec3a27f8f0da1fe2fea8a066da1
Damien Bravin <damien.bravin@inadevs.be>
2020-12-08 15:20:22 +0100

    configure artifactory for snapshot artifacts.

--- travis-ci/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] || [ -n "$TRAVIS_TAG" ]; then
-    if [ -z "$TRAVIS_TAG" ]; then
-        mvn deploy --settings travis-ci/maven_settings.xml -Dchangelist='-SNAPSHOT'
-    else
-        mvn versions:set -DnewVersion=$TRAVIS_TAG
-        mvn deploy --settings travis-ci/maven_settings.xml
-    fi
-fi
