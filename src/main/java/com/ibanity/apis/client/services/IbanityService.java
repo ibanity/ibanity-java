40db58aa1bd1627128922c3b3f939c1635b41eb7
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2021-03-29 14:36:24 +0200

    Add support for IsabelConnectService in IbanityService

--- src/main/java/com/ibanity/apis/client/services/IbanityService.java
+++ src/main/java/com/ibanity/apis/client/services/IbanityService.java
@@ -1,6 +1,7 @@
 package com.ibanity.apis.client.services;
 
 import com.ibanity.apis.client.http.IbanityHttpClient;
+import com.ibanity.apis.client.products.isabel_connect.services.IsabelConnectService;
 import com.ibanity.apis.client.products.ponto_connect.services.PontoConnectService;
 import com.ibanity.apis.client.products.xs2a.services.Xs2aService;
 
@@ -12,4 +13,6 @@ public interface IbanityService {
     Xs2aService xs2aService();
 
     PontoConnectService pontoConnectService();
+
+    IsabelConnectService isabelConnectService();
 }
