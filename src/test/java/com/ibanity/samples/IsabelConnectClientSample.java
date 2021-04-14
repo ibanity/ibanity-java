0d5c4be3053db143c76486e41ddad44b713ba9a2
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2021-04-14 15:57:16 +0200

    First version of IsabelConnectClientSample

---
+++ src/test/java/com/ibanity/samples/IsabelConnectClientSample.java
@@ -0,0 +1,79 @@
+package com.ibanity.samples;
+
+import com.ibanity.apis.client.builders.IbanityServiceBuilder;
+import com.ibanity.apis.client.builders.OptionalPropertiesBuilder;
+import com.ibanity.apis.client.helpers.IbanityClientSecuritySignaturePropertiesKeys;
+import com.ibanity.apis.client.products.isabel_connect.models.Token;
+import com.ibanity.apis.client.products.isabel_connect.models.create.TokenCreateQuery;
+import com.ibanity.apis.client.products.isabel_connect.models.refresh.TokenRefreshQuery;
+import com.ibanity.apis.client.products.isabel_connect.services.IsabelConnectService;
+import com.ibanity.apis.client.products.isabel_connect.services.TokenService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+
+import static com.ibanity.apis.client.helpers.IbanityClientSecurityAuthenticationPropertiesKeys.*;
+import static com.ibanity.apis.client.helpers.IbanityClientSecurityAuthenticationPropertiesKeys.IBANITY_CLIENT_TLS_CA_CERTIFICATE_PATH_PROPERTY_KEY;
+import static com.ibanity.apis.client.helpers.IbanityClientSecuritySignaturePropertiesKeys.*;
+import static com.ibanity.apis.client.helpers.IbanityClientSecuritySignaturePropertiesKeys.IBANITY_CLIENT_SIGNATURE_CERTIFICATE_ID_PROPERTY_KEY;
+import static com.ibanity.apis.client.helpers.IbanityConfiguration.IBANITY_API_ENDPOINT_PROPERTY_KEY;
+import static com.ibanity.apis.client.helpers.IbanityConfiguration.getConfiguration;
+import static com.ibanity.samples.helper.SampleHelper.loadCertificate;
+import static com.ibanity.samples.helper.SampleHelper.loadPrivateKey;
+
+public class IsabelConnectClientSample {
+    private static final Logger LOGGER = LogManager.getLogger(IsabelConnectClientSample.class);
+
+    private static final String clientId = getConfiguration("isabel-connect.oauth2.client_id");
+    private static final String clientSecret = getConfiguration("isabel-connect.oauth2.client_secret");
+    private static final String codeVerifier = getConfiguration("isabel-connect.oauth2.code_verifier");
+    private static final String authorizationCode = getConfiguration("isabel-connect.oauth2.authorization_code");
+
+    public static void main(String[] args) throws CertificateException, IOException {
+        String passphrase = getConfiguration(IBANITY_CLIENT_TLS_PRIVATE_KEY_PASSPHRASE_PROPERTY_KEY);
+        OptionalPropertiesBuilder ibanityServiceBuilder = IbanityServiceBuilder.builder()
+                .ibanityApiEndpoint(getConfiguration(IBANITY_API_ENDPOINT_PROPERTY_KEY))
+                .tlsPrivateKey(loadPrivateKey(getConfiguration(IBANITY_CLIENT_TLS_PRIVATE_KEY_PATH_PROPERTY_KEY), passphrase))
+                .passphrase(passphrase)
+                .tlsCertificate(loadCertificate(getConfiguration(IBANITY_CLIENT_TLS_CERTIFICATE_PATH_PROPERTY_KEY)))
+                .caCertificate(loadCertificate(getConfiguration(IBANITY_CLIENT_TLS_CA_CERTIFICATE_PATH_PROPERTY_KEY)));
+
+        if (getConfiguration(IbanityClientSecuritySignaturePropertiesKeys.IBANITY_CLIENT_SIGNATURE_CERTIFICATE_ID_PROPERTY_KEY) != null) {
+            String signaturePassphrase = getConfiguration(IBANITY_CLIENT_SIGNATURE_PRIVATE_KEY_PASSPHRASE_PROPERTY_KEY, "");
+            ibanityServiceBuilder
+                    .requestSignaturePrivateKey(loadPrivateKey(getConfiguration(IBANITY_CLIENT_SIGNATURE_PRIVATE_KEY_PATH_PROPERTY_KEY), signaturePassphrase))
+                    .requestSignaturePassphrase(signaturePassphrase)
+                    .requestSignatureCertificate(loadCertificate(getConfiguration(IBANITY_CLIENT_SIGNATURE_CERTIFICATE_PATH_PROPERTY_KEY)))
+                    .signatureCertificateId(getConfiguration(IBANITY_CLIENT_SIGNATURE_CERTIFICATE_ID_PROPERTY_KEY));
+        }
+
+        IsabelConnectService pontoConnectService = ibanityServiceBuilder
+                .pontoConnectOauth2ClientId(clientId)
+                .build()
+                .isabelConnectService();
+
+
+        String accessToken = createToken(pontoConnectService.tokenService());
+    }
+
+    private static String createToken(TokenService tokenService) {
+        LOGGER.info("Token samples");
+
+        Token token = tokenService.create(TokenCreateQuery.builder()
+                .authorizationCode(authorizationCode)
+                .clientSecret(clientSecret)
+                .codeVerifier(codeVerifier)
+                .build());
+        LOGGER.info("Token {}", token);
+
+        token = tokenService.refresh(TokenRefreshQuery.builder()
+                .clientSecret(clientSecret)
+                .refreshToken(token.getRefreshToken())
+                .build());
+        LOGGER.info("Token {}", token);
+
+        return token.getAccessToken();
+    }
+}
