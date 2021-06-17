47de61395166c385b900f5f4f5acd7bd29105b04
Christophe Leclef <christophe.leclef@ibanity.com>
2021-06-17 16:13:36 +0200

    FinancialInstitutionTransactionsService.update

---
+++ src/main/java/com/ibanity/apis/client/products/xs2a/sandbox/models/factory/update/FinancialInstitutionTransactionUpdateQuery.java
@@ -0,0 +1,40 @@
+package com.ibanity.apis.client.products.xs2a.sandbox.models.factory.create;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Builder
+@ToString
+@EqualsAndHashCode
+public class FinancialInstitutionTransactionUpdateQuery {
+    private UUID financialInstitutionId;
+    private UUID financialInstitutionUserId;
+    private UUID financialInstitutionAccountId;
+
+    private BigDecimal amount;
+    private String currency;
+
+    private Instant valueDate;
+    private Instant executionDate;
+
+    private String description;
+    private String remittanceInformationType;
+    private String remittanceInformation;
+    private String counterpartName;
+    private String counterpartReference;
+
+    private String bankTransactionCode;
+    private String proprietaryBankTransactionCode;
+    private String endToEndId;
+    private String purposeCode;
+    private String mandateId;
+    private String creditorId;
+    private String additionalInformation;
+}
