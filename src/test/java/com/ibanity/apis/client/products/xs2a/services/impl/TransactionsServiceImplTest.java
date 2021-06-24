d0cd1dfc793dc86a4c0e4d936dad4cc12f4e46c6
Christophe Leclef <christophe.leclef@ibanity.com>
2021-06-24 11:02:46 +0200

    code review

--- src/test/java/com/ibanity/apis/client/products/xs2a/services/impl/TransactionsServiceImplTest.java
+++ src/test/java/com/ibanity/apis/client/products/xs2a/services/impl/TransactionsServiceImplTest.java
@@ -2,13 +2,16 @@ package com.ibanity.apis.client.products.xs2a.services.impl;
 
 import com.ibanity.apis.client.helpers.IbanityTestHelper;
 import com.ibanity.apis.client.http.IbanityHttpClient;
-import com.ibanity.apis.client.models.*;
+import com.ibanity.apis.client.models.ErrorMeta;
+import com.ibanity.apis.client.models.FinancialInstitutionResponse;
+import com.ibanity.apis.client.models.IbanityCollection;
+import com.ibanity.apis.client.models.IbanityError;
+import com.ibanity.apis.client.models.IbanityProduct;
 import com.ibanity.apis.client.products.xs2a.models.Synchronization;
 import com.ibanity.apis.client.products.xs2a.models.Transaction;
 import com.ibanity.apis.client.products.xs2a.models.read.TransactionReadQuery;
 import com.ibanity.apis.client.products.xs2a.models.read.TransactionsReadQuery;
 import com.ibanity.apis.client.services.ApiUrlProvider;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -31,12 +34,15 @@ public class TransactionsServiceImplTest {
     private static final UUID ACCOUNT_ID = UUID.fromString("1c020714-759c-4ee6-ae87-5ce667937e77");
     private static final UUID TRANSACTION_ID = UUID.fromString("eb535c31-f619-4092-9db2-4db84149ddcb");
     private static final UUID FINANCIAL_INSTITUTION_ID = UUID.fromString("99477654-a061-414c-afb4-19e37d13c5a3");
+    private static final UUID SYNCHRONIZATION_ID = UUID.fromString("9d36e759-b606-41dd-8d18-c882bd8db03d");
 
     private static final String CUSTOMER_ACCESS_TOKEN = UUID.randomUUID().toString();
     private static final String TRANSACTION_ENDPOINT = "https://api.ibanity.com/xs2a/customer/financial-institutions/{financialInstitutionId}/accounts/{accountId}/transactions/{transactionId}";
     private static final String TRANSACTION_URI = "https://api.ibanity.com/xs2a/customer/financial-institutions/99477654-a061-414c-afb4-19e37d13c5a3/accounts/1c020714-759c-4ee6-ae87-5ce667937e77/transactions/eb535c31-f619-4092-9db2-4db84149ddcb";
     private static final String TRANSACTIONS_URI = "https://api.ibanity.com/xs2a/customer/financial-institutions/99477654-a061-414c-afb4-19e37d13c5a3/accounts/1c020714-759c-4ee6-ae87-5ce667937e77/transactions?page%5Blimit%5D=10";
     private static final String FIRST_LINK = "https://api.ibanity.com/xs2a/customer/financial-institutions/99477654-a061-414c-afb4-19e37d13c5a3/accounts/1c020714-759c-4ee6-ae87-5ce667937e77/transactions";
+    private static final String UPDATED_TRANSACTIONS_ENDPOINT = "https://api.ibanity.com/xs2a/customer/synchronizations/{synchronizationId}/updated-transactions";
+    private static final String UPDATED_TRANSACTIONS_URI = "https://api.ibanity.com/xs2a/customer/synchronizations/9d36e759-b606-41dd-8d18-c882bd8db03d/updated-transactions?page%5Blimit%5D=10";
 
     @Mock
     private ApiUrlProvider apiUrlProvider;
@@ -47,13 +53,10 @@ public class TransactionsServiceImplTest {
     @InjectMocks
     private TransactionsServiceImpl transactionsService;
 
-    @BeforeEach
-    void setUp() {
-        when(apiUrlProvider.find(IbanityProduct.Xs2a, "customer", "financialInstitution", "transactions")).thenReturn(TRANSACTION_ENDPOINT);
-    }
-
     @Test
     public void find() throws Exception {
+        when(apiUrlProvider.find(IbanityProduct.Xs2a, "customer", "financialInstitution", "transactions")).thenReturn(TRANSACTION_ENDPOINT);
+
         TransactionReadQuery transactionReadQuery =
                 TransactionReadQuery.builder()
                         .transactionId(TRANSACTION_ID)
@@ -72,7 +75,9 @@ public class TransactionsServiceImplTest {
     }
 
     @Test
-    public void list() throws Exception {
+    public void listForAccount() throws Exception {
+        when(apiUrlProvider.find(IbanityProduct.Xs2a, "customer", "financialInstitution", "transactions")).thenReturn(TRANSACTION_ENDPOINT);
+
         IbanityCollection<Transaction> expected =
                 IbanityCollection.<Transaction>builder()
                         .pageLimit(10)
@@ -111,6 +116,31 @@ public class TransactionsServiceImplTest {
 
         when(ibanityHttpClient.get(new URI(TRANSACTIONS_URI), emptyMap(), CUSTOMER_ACCESS_TOKEN))
                 .thenReturn(IbanityTestHelper.loadHttpResponse("json/transactions.json"));
+
+        IbanityCollection<Transaction> actual = transactionsService.list(transactionReadQuery);
+
+        assertThat(actual).isEqualToComparingFieldByFieldRecursively(expected);
+    }
+
+    @Test
+    public void listUpdatedForSynchronization() throws Exception {
+        when(apiUrlProvider.find(IbanityProduct.Xs2a, "customer", "synchronization", "updatedTransactions")).thenReturn(UPDATED_TRANSACTIONS_ENDPOINT);
+
+        IbanityCollection<Transaction> expected =
+                IbanityCollection.<Transaction>builder()
+                        .pageLimit(10)
+                        .firstLink("https://api.ibanity.com/xs2a/customer/synchronizations/9d36e759-b606-41dd-8d18-c882bd8db03d/updated-transactions")
+                        .items(newArrayList(createExpected()))
+                        .build();
+
+        TransactionsReadQuery transactionReadQuery =
+                TransactionsReadQuery.builder()
+                        .synchronizationId(SYNCHRONIZATION_ID)
+                        .customerAccessToken(CUSTOMER_ACCESS_TOKEN)
+                        .build();
+
+        when(ibanityHttpClient.get(new URI(UPDATED_TRANSACTIONS_URI), emptyMap(), CUSTOMER_ACCESS_TOKEN))
+                .thenReturn(IbanityTestHelper.loadHttpResponse("json/updated-transactions.json"));
 
         IbanityCollection<Transaction> actual = transactionsService.list(transactionReadQuery);
 
