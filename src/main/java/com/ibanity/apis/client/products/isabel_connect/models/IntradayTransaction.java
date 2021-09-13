febc9444d008cede5208bbde815e0859fd73c9ea
Gilles Hooghe <gilles.hooghe@ibanity.com>
2021-09-13 16:21:07 +0200

    Fix IntradayTransaction unit test

--- src/main/java/com/ibanity/apis/client/products/isabel_connect/models/IntradayTransaction.java
+++ src/main/java/com/ibanity/apis/client/products/isabel_connect/models/IntradayTransaction.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
-import java.time.Instant;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
