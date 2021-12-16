9c005a775e2693383bb4cdf0c91a89c1d4cab257
Damien Bravin <damien.bravin@inadevs.be>
2021-12-16 13:58:47 +0100

    Add PaymentActivationRequest service and models.

--- src/main/java/com/ibanity/apis/client/products/ponto_connect/services/PontoConnectService.java
+++ src/main/java/com/ibanity/apis/client/products/ponto_connect/services/PontoConnectService.java
@@ -29,4 +29,6 @@ public interface PontoConnectService {
     BulkPaymentService bulkPaymentService();
 
     ReauthorizationRequestService reauthorizationRequestService();
+
+    PaymentActivationRequestService paymentActivationRequestService();
 }
