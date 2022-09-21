172bea22fb2067937e7edba94c935e29e1f82e98
Damien Bravin <damien.bravin@inadevs.be>
2022-09-21 10:44:16 +0200

    Add additional Ponto Connect webhooks.

---
+++ src/main/java/com/ibanity/apis/client/webhooks/models/ponto_connect/AccountReauthorized.java
@@ -0,0 +1,52 @@
+package com.ibanity.apis.client.webhooks.models.ponto_connect;
+
+import com.ibanity.apis.client.jsonapi.DataApiModel;
+import com.ibanity.apis.client.jsonapi.RelationshipsApiModel;
+import com.ibanity.apis.client.models.IbanityWebhookEvent;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Instant;
+import java.util.UUID;
+import java.util.function.Function;
+
+import static com.ibanity.apis.client.mappers.IbanityWebhookEventMapper.toIbanityWebhooks;
+import static java.util.UUID.fromString;
+
+@Data
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AccountReauthorized implements IbanityWebhookEvent {
+
+    public final static String TYPE = "pontoConnect.account.reauthorized";
+
+    private UUID id;
+    private String type;
+    private UUID account;
+    private UUID organization;
+    private Instant createdAt;
+
+    public static Function<DataApiModel, AccountReauthorized> mappingFunction() {
+        return dataApiModel -> {
+            AccountReauthorized integrationAccountAdded = toIbanityWebhooks(dataApiModel, AccountReauthorized.class);
+
+            RelationshipsApiModel organizationRelationship = dataApiModel.getRelationships().get("organization");
+            if (organizationRelationship != null) {
+                integrationAccountAdded.setOrganization(fromString(organizationRelationship.getData().getId()));
+            }
+
+
+            RelationshipsApiModel accountRelationship = dataApiModel.getRelationships().get("account");
+            if (accountRelationship != null) {
+                integrationAccountAdded.setAccount(fromString(accountRelationship.getData().getId()));
+            }
+
+            return integrationAccountAdded;
+        };
+    }
+
+}
---
+++ src/main/java/com/ibanity/apis/client/webhooks/models/ponto_connect/IntegrationRevoked.java
@@ -0,0 +1,45 @@
+package com.ibanity.apis.client.webhooks.models.ponto_connect;
+
+import com.ibanity.apis.client.jsonapi.DataApiModel;
+import com.ibanity.apis.client.jsonapi.RelationshipsApiModel;
+import com.ibanity.apis.client.models.IbanityWebhookEvent;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Instant;
+import java.util.UUID;
+import java.util.function.Function;
+
+import static com.ibanity.apis.client.mappers.IbanityWebhookEventMapper.toIbanityWebhooks;
+import static java.util.UUID.fromString;
+
+@Data
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class IntegrationRevoked implements IbanityWebhookEvent {
+
+    public final static String TYPE = "pontoConnect.integration.revoked";
+
+    private UUID id;
+    private String type;
+    private UUID organization;
+    private Instant createdAt;
+
+    public static Function<DataApiModel, IntegrationRevoked> mappingFunction() {
+        return dataApiModel -> {
+            IntegrationRevoked integrationRevoked = toIbanityWebhooks(dataApiModel, IntegrationRevoked.class);
+
+            RelationshipsApiModel organizationRelationship = dataApiModel.getRelationships().get("organization");
+            if (organizationRelationship != null) {
+                integrationRevoked.setOrganization(fromString(organizationRelationship.getData().getId()));
+            }
+
+            return integrationRevoked;
+        };
+    }
+
+}
