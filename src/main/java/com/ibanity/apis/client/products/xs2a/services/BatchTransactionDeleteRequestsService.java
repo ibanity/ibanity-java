5a7091459b93cf9b26598241770c57beda22ac74
Christophe Leclef <christophe.leclef@ibanity.com>
2023-07-07 15:54:33 +0200

    fix naming, include in xs2aService

--- src/main/java/com/ibanity/apis/client/products/xs2a/services/BatchTransactionDeleteRequestsService.java
+++ src/main/java/com/ibanity/apis/client/products/xs2a/services/BatchTransactionDeleteRequestsService.java
@@ -3,7 +3,7 @@ package com.ibanity.apis.client.products.xs2a.services;
 import com.ibanity.apis.client.products.xs2a.models.BatchTransactionDeleteRequest;
 import com.ibanity.apis.client.products.xs2a.models.create.BatchTransactionDeleteRequestCreationQuery;
 
-public interface BatchTransactionDeleteRequestService {
+public interface BatchTransactionDeleteRequestsService {
 
     BatchTransactionDeleteRequest create(BatchTransactionDeleteRequestCreationQuery batchTransactionDeleteRequestCreationQuery);
 }
--- src/main/java/com/ibanity/apis/client/products/xs2a/services/TransactionDeleteRequestsService.java
+++ src/main/java/com/ibanity/apis/client/products/xs2a/services/TransactionDeleteRequestsService.java
@@ -3,7 +3,7 @@ package com.ibanity.apis.client.products.xs2a.services;
 import com.ibanity.apis.client.products.xs2a.models.TransactionDeleteRequest;
 import com.ibanity.apis.client.products.xs2a.models.create.TransactionDeleteRequestCreationQuery;
 
-public interface TransactionDeleteRequestService {
+public interface TransactionDeleteRequestsService {
 
     TransactionDeleteRequest create(TransactionDeleteRequestCreationQuery transactionDeleteRequestCreationQuery);
 }
