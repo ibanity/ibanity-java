6cc0d48f61b28b4224ecfe6c8f0855373882ea43
Damien Bravin <damien.bravin@ibanity.com>
2024-03-21 16:03:08 +0100

    Remove unused beanutils dependency.

--- src/main/java/com/ibanity/apis/client/mappers/IsabelModelMapper.java
+++ src/main/java/com/ibanity/apis/client/mappers/IsabelModelMapper.java
@@ -8,14 +8,12 @@ import com.ibanity.apis.client.products.isabel_connect.models.IsabelModel;
 import com.ibanity.apis.client.utils.IbanityUtils;
 import org.apache.http.HttpResponse;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
-import java.io.IOException;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import org.apache.commons.beanutils.PropertyUtils;
 
 import static com.ibanity.apis.client.mappers.ModelMapperHelper.getRequestId;
 import static com.ibanity.apis.client.mappers.ModelMapperHelper.readResponseContent;
