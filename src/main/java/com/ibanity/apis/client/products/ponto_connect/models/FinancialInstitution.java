e13dcf4ff7984676cf440d26a3afbd815158513f
ugo.lambert <ugo.lambert@ibanity.com>
2025-11-04 15:02:55 +0100

    PO-1830 Add Missing fields in financial institution

--- src/main/java/com/ibanity/apis/client/products/ponto_connect/models/FinancialInstitution.java
+++ src/main/java/com/ibanity/apis/client/products/ponto_connect/models/FinancialInstitution.java
@@ -27,5 +27,22 @@ public class FinancialInstitution implements IbanityModel {
     private String status;
     private boolean deprecated;
     private String timeZone;
+    private boolean bulkPaymentsEnabled;
+    private String bulkPaymentsProductTypes;
+    private boolean futureDatedPaymentsAllowed;
+    private String maintenanceFrom;
+    private String maintenanceTo;
+    private String maintenanceType;
+    private boolean paymentsEnabled;
+    private String paymentsProductTypes;
+    private boolean periodicPaymentsEnabled;
+    private String periodicPaymentsProductTypes;
+    private String sharedBrandName;
+    private String sharedBrandReference;
+    private boolean pendingTransactionsAvailable;
+    private Integer bulkPaymentInstructionsLimit;
+    private Integer expectedAuthorizationLifetime;
+    private boolean paymentDebtorAccountReferenceRequired;
+    private boolean bulkPaymentDebtorAccountReferenceRequired;
 }
 
